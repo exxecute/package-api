8be73711a809d09da14904aeb9d78a9c34863bff
3x3cvte <3xecvte@gmail.com>
2023-10-06 10:02:07 +0300

    fixed: buffer sintax, buffer size init edit: massive printing %02X

--- main.c
+++ main.c
@@ -34,6 +34,7 @@ static uint8_t _validate_head(const uint8_t *__buffer, const uint8_t *__head, co
 void PACKAGE_API_init(PACKAGE_API_t *this, uint8_t* __buffer, uint16_t __buffer_size)
 {
     memcpy(this->buffer, __buffer, __buffer_size);
+    this->buffer_size = __buffer_size;
 }
 
 void PACKAGE_API_find_package(PACKAGE_API_t *this)
@@ -61,17 +62,17 @@ void PACKAGE_API_find_package(PACKAGE_API_t *this)
     }
 }
 
-static void _print_massive(int *__massive, int __size, char *__name)
+static void _print_massive(uint8_t *__massive, uint8_t __size, char *__name)
 {
 	printf("%s: ", __name);
 	for(int i = 0; i < __size; i++)
 	{
-		printf("%d ", __massive[i]);
+		printf("%02X ", __massive[i]);
 	}
 	printf("\n");
 }
 
-const uint8_t test_buffer = {0x00, 0x00, 0x00, 0x00, 0x11, 0x22, 0x00, 0x11, 0x22, 0x33, \
+const uint8_t test_buffer[] = {0x00, 0x00, 0x00, 0x00, 0x11, 0x22, 0x00, 0x11, 0x22, 0x33, \
                             0x00, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77, 0x88, 0x99,\
                             0xcc, 0xdd, 0xee, 0xcc, 0xdd, 0xee, 0xff};
 const uint16_t test_size = 27;
@@ -79,11 +80,12 @@ const uint16_t test_size = 27;
 int main(void)
 {
     PACKAGE_API_t package_api = {0};
-    printf("1");
+    _print_massive(test_buffer, 28, "test buffer");
+    _print_massive(package_api.buffer, 64, "buffer before cpy");
     PACKAGE_API_init(&package_api, test_buffer, test_size);
-    printf("2");
+    _print_massive(package_api.buffer, 64, "buffer after cpy");
     PACKAGE_API_find_package(&package_api);
-    printf("3");
-    _print_massive(package_api.buffer + package_api.start_package_byte, package_api.stop_package_byte - package_api.start_package_byte, "buffer");
+    printf("start byte: %d, stop byte: %d\n", package_api.start_package_byte, package_api.stop_package_byte);
+    _print_massive(package_api.buffer + package_api.start_package_byte, package_api.stop_package_byte - package_api.start_package_byte, "package");
     return 1;
 }
